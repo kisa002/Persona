0b0b5f8423843a075cd4735bc82d43e2c56c89bd
kisa002 <vnycall74@naver.com>
2017-11-25 17:20:02 +0900

    결과창 구현

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -6,25 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
-    public int timer = 60;
-    Text textTimer;
-
+    //Gauge
     Gauge[] gauge = new Gauge[4];
+
     public float[] value = new float[4];
     static public float gaugeAll = 0;
 
-    public Text[] textStart = new Text[3];
-    public Image[] imageStart = new Image[3];
+
+    //Timer
+    public int timer = 60;
+    Text textTimer;
+
+
+    //Result
+    public bool isFinish = false;
+    
+    GameObject panelResult;
+    GameObject[] resultPlayer = new GameObject[4];
 
     void Start ()
     {
         if (SceneManager.GetActiveScene().name == "Main")
         {
-            for (int i = 0; i < 3; i++)
-            {
-                textStart[i] = GameObject.Find("Start" + (i + 2)).transform.FindChild("Text").GetComponent<Text>();
-                imageStart[i] = GameObject.Find("Start" + (i + 2)).GetComponent<Image>();
-            }
+
         }
         if (SceneManager.GetActiveScene().name == "Game")
         {
@@ -36,29 +40,52 @@ public class UIManager : MonoBehaviour {
                 gauge[i] = new Gauge();
 
                 gauge[i].obj = GameObject.Find("Gauge" + (i + 1));
+                gauge[i].textPercent = gauge[i].obj.transform.FindChild("TextPercent").GetComponent<Text>();
             }
+
+            panelResult = GameObject.Find("PanelResult");
+
+            for (int i = 0; i < 4; i++)
+                resultPlayer[i] = panelResult.transform.FindChild("Player" + (i + 1)).gameObject;
+
+            panelResult.active = false;
         }
 	}
 	
 	void Update () {
         if (SceneManager.GetActiveScene().name == "Game")
         {
-            CheckGauge();
-
-            for (int i = 0; i < 4; i++)
+            if(!isFinish)
             {
-                gauge[i].value = value[i];
-                gauge[i].ChangeWidth();
+                CheckGauge();
 
-                if (i == 0)
+                for (int i = 0; i < 4; i++)
                 {
-                    gauge[i].obj.transform.position = new Vector2(0, 1000f);
-                    //Debug.Log("Gauge" + i + " - (POS)" + gauge[i].obj.transform.position + " / (WIDTH)" + gauge[i].obj.GetComponent<RectTransform>().rect.width);
+                    gauge[i].value = value[i];
+                    gauge[i].ChangeWidth();
+
+                    if (i == 0)
+                        gauge[i].obj.transform.position = new Vector2(0, 1000f);
+                    else
+                        gauge[i].obj.transform.position = new Vector2(gauge[i - 1].obj.transform.position.x + gauge[i - 1].obj.GetComponent<RectTransform>().rect.width, 1000f);
                 }
-                else
+            }
+            else
+            {
+                if(!panelResult.active)
                 {
-                    gauge[i].obj.transform.position = new Vector2(gauge[i - 1].obj.transform.position.x + gauge[i - 1].obj.GetComponent<RectTransform>().rect.width, 1000f);
-                    Debug.Log("Gauge" + (i - 1) + " - (POS)" + gauge[i - 1].obj.transform.position + " / (WIDTH)" + gauge[i - 1].obj.GetComponent<RectTransform>().rect.width);
+                    panelResult.active = true;
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        resultPlayer[i].transform.FindChild("Gauge").transform.FindChild("TextPercent").GetComponent<Text>().text = Mathf.Round(gauge[i].percent).ToString() + "%";
+
+                        gauge[i].obj = resultPlayer[i].transform.FindChild("Gauge").gameObject;
+                        gauge[i].ResultWidth();
+                    }
+
+                    //GameObject.Find("ImageTimer").active = false;
+                    //GameObject.Find("TextTimer").active = false;
                 }
             }
         }
@@ -92,6 +119,8 @@ public class UIManager : MonoBehaviour {
     class Gauge
     {
         public GameObject obj;
+        public Text textPercent;
+
         public float value = 0;
 
         public float percent = 0;
@@ -103,15 +132,19 @@ public class UIManager : MonoBehaviour {
                 percent = (value / gaugeAll) * 100f;
 
                 obj.GetComponent<RectTransform>().sizeDelta = new Vector2((percent / 100f) * 1920f, obj.GetComponent<RectTransform>().rect.height);
+            }
 
-                //Debug.Log(obj.name + " : " + value + " / " + gaugeAll + " =  " + percent + "%");
+            textPercent.text = Mathf.Round(percent).ToString() + "%";
+        }
+
+        public void ResultWidth()
+        {
+            if (gaugeAll > 0)
+            {
+                percent = (value / gaugeAll) * 100f;
+
+                obj.GetComponent<RectTransform>().sizeDelta = new Vector2((percent / 100f) * 1000f, obj.GetComponent<RectTransform>().rect.height);
             }
         }
-    }
-
-    public void OnPlayButton()
-    {
-        Debug.Log(this.gameObject.name);
-        
     }
 }
